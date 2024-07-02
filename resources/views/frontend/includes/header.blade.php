2afc41a3584724dcf222756fa2bfaf3572808fd6
unknown <sujandangi171@gmail.com>
2024-07-02 16:34:45 +0545

    changes

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -1,8 +1,9 @@
 <!-- navigation start -->
 <nav id="service-section">
     <div class="logo">
-        <a href="{{ route('frontend.home') }}"><img src="{{ asset('uploads/config/' . getConfig('site-logo')) }}"
-                alt="ENDEAVOR Nepal"></a>
+        <a href="{{ route('frontend.home') }}">
+            <img src="{{ asset('uploads/config/' . getConfig('site-logo')) }}" alt="ENDEAVOR Nepal">
+        </a>
     </div>
     <input type="checkbox" id="check">
     <label for="check">
@@ -11,21 +12,24 @@
     </label>
     <ul class="nav-links">
         @foreach ($menus as $menu)
-        @if (count($menu->children) > 0)
-        <li>
-            <div class="dropdown">
-                <a class="scroll-link" href="#{{ $menu->href }}">{{ $menu->title_eng }} <span
-                        class="material-symbols-outlined arrow">▾</span></a>
-                <div class="dropdown-content">
-                    @foreach ($menu->children as $child)
-                    <a href="/{{ $child->href }}">▸ {{ $child->title_eng }}</a>
-                    @endforeach
-                </div>
-            </div>
-        </li>
-        @else
-        <li><a href="/#{{ $menu->href }}">{{ $menu->title_eng }}</a></li>
-        @endif
+            @if (count($menu->children) > 0)
+                <li>
+                    <div class="dropdown">
+                        <a class="scroll-link" href="#{{ $menu->href }}">{{ $menu->title_eng }}
+                            <span class="material-symbols-outlined arrow">▾</span>
+                        </a>
+                        <div class="dropdown-content">
+                            @foreach ($menu->children as $child)
+                                <a href="/{{ $child->href }}">▸ {{ $child->title_eng }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </li>
+            @else
+                @if (!$menu->is_child)
+                    <li><a href="/#{{ $menu->href }}">{{ $menu->title_eng }}</a></li>
+                @endif
+            @endif
         @endforeach
     </ul>
 </nav>
